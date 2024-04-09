cb4131dcee78d5e0f23ba002661218410b021eab
avelando <avelarrodrigues89@gmail.com>
2024-04-09 02:37:36 -0300

    uri alterada

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         auth0Client = await createAuth0Client({
             domain: 'dev-6je86woartrc1pi1.us.auth0.com',
             client_id: 'W167A9iGNOl6wtYnkNAkFbgkmclnFf4W',
-            redirect_uri: 'https://avelando.github.io/js-dom-api/callback'
+            redirect_uri: 'https://avelando.github.io/js-dom-api/'
         });
 
         if (window.location.search.includes('code=')) {
